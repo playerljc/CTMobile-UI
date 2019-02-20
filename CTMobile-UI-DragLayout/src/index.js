0b4a83585b5788bed6fbcdb93a40c73fb44d20c1
zhiqian.liu <1qaz@WSX>
2019-02-20 19:55:12 +0800

    style(common): 代码的提交
    1.CTMobile-UI-DragLlayout加入分包

---
+++ CTMobile-UI-DragLayout/src/index.js
@@ -0,0 +1,4 @@
+import Draglayout from './draglayout';
+import './index.less';
+
+export { Draglayout };
---
+++ CTMobile-UI-DragLayout/standard/lib/index.js
@@ -0,0 +1,2 @@
+"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"Draglayout",{enumerable:!0,get:function(){return _draglayout.default}});var _draglayout=_interopRequireDefault(require("./draglayout"));require("./index.less");
+//# sourceMappingURL=index.js.map
