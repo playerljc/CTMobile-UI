9fe25e60bd2f9d68e83c814b7b0fa6efea80709c
zhiqian.liu <1qaz@WSX>
2019-02-21 10:18:08 +0800

    style(common): 代码的提交
    1.CTMobile-UI-Notification加入分包

---
+++ CTMobile-UI-Notification/release/lib/index.js
@@ -0,0 +1,2 @@
+"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"Notification",{enumerable:!0,get:function(){return _notification.default}});var _notification=_interopRequireDefault(require("./notification"));require("./index.less");
+//# sourceMappingURL=index.js.map
---
+++ CTMobile-UI-Notification/src/index.js
@@ -0,0 +1,4 @@
+import Notification from './notification';
+import './index.less';
+
+export { Notification };
