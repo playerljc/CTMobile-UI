bd7bde8e37942771cf55efc5554d92e22399d31f
zhiqian.liu <1qaz@WSX>
2019-02-25 16:25:18 +0800

    style(common): 代码的提交
    1.CTMobile-UI-KwList加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-KwList/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"KwList",{enumerable:!0,get:function(){return _kwlist.default}});var _kwlist=_interopRequireDefault(require("./kwlist"));require("./index.less");
-//# sourceMappingURL=index.js.map
