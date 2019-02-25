6b607463f17471827cb35b102d8465b4e043ccd1
zhiqian.liu <1qaz@WSX>
2019-02-25 16:43:23 +0800

    style(common): 代码的提交
    1.CTMobile-UI-Popup加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-Popup/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"Popup",{enumerable:!0,get:function(){return _popup.default}});var _popup=_interopRequireDefault(require("./popup"));require("./index.less");
-//# sourceMappingURL=index.js.map
