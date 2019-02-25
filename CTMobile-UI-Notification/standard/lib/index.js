b8e57abd819792d2dd998c2ed4e7dc696e8f782f
zhiqian.liu <1qaz@WSX>
2019-02-25 16:39:21 +0800

    style(common): 代码的提交
    1.CTMobile-UI-Notification加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-Notification/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"Notification",{enumerable:!0,get:function(){return _notification.default}});var _notification=_interopRequireDefault(require("./notification"));require("./index.less");
-//# sourceMappingURL=index.js.map
