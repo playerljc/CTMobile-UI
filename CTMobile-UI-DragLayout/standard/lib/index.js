f705bf0176458a3588fc7bb9b81bc3f059449551
zhiqian.liu <1qaz@WSX>
2019-02-25 16:01:14 +0800

    style(common): 代码的提交
    1.CTMobile-UI-DragLayout加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-DragLayout/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"Draglayout",{enumerable:!0,get:function(){return _draglayout.default}});var _draglayout=_interopRequireDefault(require("./draglayout"));require("./index.less");
-//# sourceMappingURL=index.js.map
