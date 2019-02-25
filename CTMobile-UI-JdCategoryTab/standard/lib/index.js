691629d3125244c94eec2e5af430f8be00041513
zhiqian.liu <1qaz@WSX>
2019-02-25 16:21:36 +0800

    style(common): 代码的提交
    1.CTMobile-UI-JdCategoryTab加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-JdCategoryTab/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"JdCategoryTab",{enumerable:!0,get:function(){return _jdcategorytab.default}});var _jdcategorytab=_interopRequireDefault(require("./jdcategorytab"));require("./index.less");
-//# sourceMappingURL=index.js.map
