3c3e2cc87403b2bb11a54b47d00f27fb794e3568
zhiqian.liu <1qaz@WSX>
2019-02-25 16:46:52 +0800

    style(common): 代码的提交
    1.CTMobile-UI-StickupLayout加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-StickupLayout/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"StickupLayout",{enumerable:!0,get:function(){return _stickuplayout.default}});var _stickuplayout=_interopRequireDefault(require("./stickuplayout"));require("./index.less");
-//# sourceMappingURL=index.js.map
