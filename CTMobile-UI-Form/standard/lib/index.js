61523b960b27c14c5937e6a409a9872047cb17e5
zhiqian.liu <1qaz@WSX>
2019-02-25 16:11:42 +0800

    style(common): 代码的提交
    1.CTMobile-UI-Form加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-Form/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"Form",{enumerable:!0,get:function(){return _form.default}});var _form=_interopRequireDefault(require("./form"));require("./index.less");
-//# sourceMappingURL=index.js.map
