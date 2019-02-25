bcbdd57e5dedf811ab0d25792e5dc7288ff83370
zhiqian.liu <1qaz@WSX>
2019-02-25 16:17:41 +0800

    style(common): 代码的提交
    1.CTMobile-UI-ImageViewer加入release和standard不同less文件的不同打包处理

--- CTMobile-UI-ImageViewer/standard/lib/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"ImageViewer",{enumerable:!0,get:function(){return _imageviewer.default}});var _imageviewer=_interopRequireDefault(require("./imageviewer"));require("./index.less");
-//# sourceMappingURL=index.js.map
