95959d9365e6ff0dfec6771ba355a319f2ad99f8
zhiqian.liu <1qaz@WSX>
2019-07-21 18:38:16 +0800

    style(common): 代码的提交
    加入自动化生成build的脚本

---
+++ script/builddemo.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const { spawn } = require('child_process');
+const components = require('../package.json').components;
+
+// 运行命令的路径
+const runtimePath = process.cwd();
+const binPath = path.join(runtimePath, 'node_modules', '.bin');
+// 命令的路径
+// const codePath = __dirname;
+
+let index = 0;
+
+function copy(componentName) {
+  return new Promise((resolve, reject) => {
+    const taskProcess = spawn(process.platform === 'win32' ? path.join(binPath, 'ctcopy.cmd') : path.join(binPath, 'ctcopy'),
+      [
+        'dist',
+        `../../../playerljc.github.io/ctmobile-ui/html/${componentName}`,
+      ],
+      {
+        cwd: path.join(runtimePath, `CTMobile-UI-${componentName}`, 'demo'),
+        encoding: 'utf-8',
+      });
+
+    taskProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    taskProcess.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    taskProcess.on('close', (code) => {
+      resolve();
+    });
+  });
+}
+
+function task(componentName) {
+  return new Promise((resolve, reject) => {
+    const demoPath = path.join(runtimePath, `CTMobile-UI-${componentName}`, 'demo');
+    const taskProcess = spawn(process.platform === 'win32' ? path.join(binPath, 'ctbuild.cmd') : path.join(binPath, 'ctbuild'),
+      [
+        '--type',
+        'buildapp',
+      ],
+      {
+        cwd: demoPath,
+        encoding: 'utf-8',
+      });
+
+    taskProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    taskProcess.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    taskProcess.on('close', (code) => {
+      console.log(`link：${code}`);
+      copy(componentName).then(() => {
+        resolve();
+      }).catch(() => {
+        reject();
+      });
+    });
+  });
+}
+
+/**
+ * loopTask
+ * @return {Promise}
+ */
+function loopTask() {
+  return new Promise((resolve, reject) => {
+    if (index >= components.length) {
+      resolve();
+    } else {
+      const componentName = components[index++];
+      if (componentName) {
+        task(componentName).then(() => {
+          loopTask().then(() => {
+            resolve();
+          });
+        }).catch((error) => {
+          reject(error);
+        });
+      } else {
+        reject();
+      }
+    }
+  });
+}
+
+function build() {
+  loopTask().then(() => {
+    console.log('finish');
+    process.exit();
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
+build();
